8fd3dd85829ee786e446162995672d0081c64118
yuriy_pavlov <yurixadm@gmail.com>
2018-05-23 12:26:35 +0300

    clean terraform examples

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 0.11.3"
 
   backend "s3" {
-    bucket = "epam-migration-staging-virginia-terraform-state"
-    key    = "epam-migration-staging-virginia-discovery-stack"
+    bucket = ""
+    key    = ""
   }
 }
